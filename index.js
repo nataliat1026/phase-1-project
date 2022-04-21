fe2baf7bf66bbff263814faa3ab86de22f4f2b5a
Natalia Teixeira <nataliat1026@gmail.com>
2022-04-20 20:42:05 -0400

    fave and kill list progress

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ const renderGallery = (charArr) => {
     })
 }
 
-
 const enlargeImg = (img) => {
     img.style.opacity = '0.7';
 }
@@ -35,16 +34,25 @@ const createModal = (charObj) => {
     const pName = document.createElement('p');
     const pTitle = document.createElement('p');
     const pHouse = document.createElement('p');
-    const span = document.querySelector('.close');
+    const close = document.querySelector('.close');
+    const heart = document.querySelector('.heart');
     modalCont.style.display = 'block';
     modalImg.src = charObj.imageUrl;
     pName.textContent = charObj.fullName;
     pTitle.textContent = charObj.title;
     pHouse.textContent = charObj.family;
     modalCap.append(pName, pTitle, pHouse);
-    span.addEventListener('click', () => {
+    close.addEventListener('click', () => {
         modalCont.style.display = 'none';
         modalCap.textContent = '';
     })
-}
+    heart.addEventListener('click', () => {
+        modalCont.style.display = 'none';
+        const favDiv = document.querySelector('#favDiv');
+        const favImg = document.createElement('img');
+        favImg.src = modalImg.src;
+        console.log(favImg);
+        favDiv.append(favImg);
 
+    })
+}
